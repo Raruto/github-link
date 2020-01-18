ba7af6568dce39d134c34e1d0238ef81f4c6258e
Raruto <Theo.95@hotmail.it>
2020-01-18 23:40:48 +0100

    code refactoring

--- github-link.php
+++ github-link.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name:       GitHub Link
-Version:           0.4.6
+Version:           1.0.0
 Plugin URI:        https://github.com/szepeviktor/github-link
-Description:       Displays GitHub link on the Plugins page given there is a <code>GitHub Plugin URI</code> plugin header.
+Description:       Displays GitHub icon link on the Plugins page for a given <code>GitHub Plugin URI</code> plugin header.
 License:           The MIT License (MIT)
 Author:            Viktor Szépe
 Domain Path:       /languages
@@ -11,170 +11,158 @@ Text Domain:       github-link
 GitHub Plugin URI: https://github.com/szepeviktor/github-link
 */
 
-if ( ! function_exists( 'add_filter' ) ) {
-    error_log( 'Malicious traffic detected: github_link_direct_access '
-        . addslashes( $_SERVER['REQUEST_URI'] )
-    );
-    ob_get_level() && ob_end_clean();
-    header( 'Status: 403 Forbidden' );
-    header( 'HTTP/1.0 403 Forbidden' );
-    exit();
-}
-
-load_plugin_textdomain( 'github-link', false, dirname( __FILE__ ) . '/languages' );
-
-add_filter( "extra_plugin_headers", "GHL_extra_headers" );
-add_filter( "plugin_action_links", "GHL_plugin_link", 10, 4 );
-add_filter( "network_admin_plugin_action_links", "GHL_plugin_link", 10, 4 );
-add_action( "admin_head-plugins.php", "GHL_wp49_css_fix" );
-
-function GHL_extra_headers( $extra_headers ) {
-
-    // Keys will get lost.
-    return array_merge( $extra_headers, array(
-        "GitHubURI" => "GitHub Plugin URI",
-        "GitHubBranch" => "GitHub Branch",
-        "GitHubToken" => "GitHub Access Token",
-        "GitLabURI" => "GitLab Plugin URI",
-        "GitLabBranch" => "GitLab Branch",
-        "BitbucketURI" => "Bitbucket Plugin URI",
-        "BitbucketBranch" => "Bitbucket Branch"
-    ) );
-}
-
-function GHL_plugin_link( $actions, $plugin_file, $plugin_data, $context ) {
-
-    // No GitHub data during search installed plugins.
-    if ( 'search' === $context )
-        return $actions;
-
-    $link_template = '<a href="%s" title="%s" target="_blank"><img src="%s" style="width: 16px; height: 16px; vertical-align:-3px;" height="16" width="16" alt="%s" />%s</a>';
-    $wp_link_template = '<a href="%s" title="%s" target="_blank"><span style="width: 16px; height: 16px; color:#2880A8; font-size:18px; vertical-align:-3px;" class="github-link dashicons dashicons-wordpress"></span></a>';
-
-    $on_wporg = false;
-    _maybe_update_plugins();
-    $plugin_state = get_site_transient( 'update_plugins' );
-    if ( isset( $plugin_state->response[ $plugin_file ] )
-        || isset( $plugin_state->no_update[ $plugin_file ] )
-    ) {
-        $on_wporg = true;
-    }
-
-    if ( ! empty( $plugin_data["GitHub Plugin URI"] ) ) {
-        $icon = "icon/GitHub-Mark-32px.png";
-        $branch = '';
-
-        if ( ! empty( $plugin_data["GitHub Access Token"] ) )
-            $icon = 'icon/GitHub-Mark-Private-32px.png"';
-        if ( ! empty( $plugin_data["GitHub Branch"] ) )
-            $branch = '/' . $plugin_data["GitHub Branch"];
-
-        $new_action = array ( 'github' => sprintf(
-            $link_template,
-            $plugin_data["GitHub Plugin URI"],
-            __( "Visit GitHub repository" , "github-link" ),
-            plugins_url( $icon, __FILE__ ),
-            "GitHub",
-            $branch
-        ) );
-        // If on WP.org + master -> put the icon after other actions.
-        if ( $on_wporg && ( empty( $branch ) || '/master' === $branch ) ) {
-            $actions = array_merge( $actions, $new_action );
-        } else {
-            $actions = array_merge( $new_action, $actions );
-        }
-    }
-
-    if ( ! empty( $plugin_data["GitLab Plugin URI"] ) ) {
-        $icon = "icon/GitLab-Mark-32px.png";
-        $branch = '';
-
-        if ( ! empty( $plugin_data["GitLab Branch"] ) )
-            $branch = '/' . $plugin_data["GitLab Branch"];
-
-        $new_action = array ( 'gitlab' => sprintf(
-            $link_template,
-            $plugin_data["GitLab Plugin URI"],
-            __( "Visit GitLab repository" , "github-link" ),
-            plugins_url( $icon, __FILE__ ),
-            "GitLab",
-            $branch
-        ) );
-        // If on WP.org + master -> put the icon after other actions.
-        if ( $on_wporg && ( empty( $branch ) || '/master' === $branch ) ) {
-            $actions = array_merge( $actions, $new_action );
-        } else {
-            $actions = array_merge( $new_action, $actions );
-        }
-    }
-
-    if ( ! empty( $plugin_data["Bitbucket Plugin URI"] ) ) {
-        $icon = "icon/bitbucket_32_darkblue_atlassian.png";
-        $branch = '';
-
-        if ( ! empty( $plugin_data["Bitbucket Branch"] ) )
-            $branch = '/' . $plugin_data["Bitbucket Branch"];
-
-        $new_action = array( 'bitbucket' => sprintf(
-            $link_template,
-            $plugin_data["Bitbucket Plugin URI"],
-            __( "Visit Bitbucket repository" , "github-link" ),
-            plugins_url( $icon, __FILE__ ),
-            "Bitbucket",
-            $branch
-        ) );
-        // If on WP.org + master -> put the icon after other actions.
-        if ( $on_wporg && ( empty( $branch ) || '/master' === $branch ) ) {
-            $actions = array_merge( $actions, $new_action );
-        } else {
-            $actions = array_merge( $new_action, $actions );
-        }
-    }
-
-    if ( $on_wporg ) {
-        $plugin_page = '';
-        if ( isset( $plugin_state->response[ $plugin_file ] ) ) {
-            if ( property_exists( $plugin_state->response[ $plugin_file ], 'url' ) ) {
-                $plugin_page = $plugin_state->response[ $plugin_file ]->url;
-            }
-        } elseif ( isset( $plugin_state->no_update[ $plugin_file ] ) ) {
-            if ( property_exists( $plugin_state->no_update[ $plugin_file ], 'url' ) ) {
-                $plugin_page = $plugin_state->no_update[ $plugin_file ]->url;
-            }
-        }
-
-        // GHU also sets plugin->url.
-        if ( false !== strstr( $plugin_page, '//wordpress.org/plugins/' ) ) {
-            $new_action = array( 'wordpress_org' => sprintf(
-                $wp_link_template,
-                $plugin_page,
-                __( "Visit WordPress.org Plugin Page" , "github-link" )
-            ) );
-            $actions = array_merge( $new_action, $actions );
-        }
-    }
-
-    return $actions;
-}
-
 /**
- * In WordPress 4.9, core introduced icons next to plugin on the update-code.php
- * page. This adds a new font-size to the pseudo :before of 60px. This CSS line
- * fixes it for the plugin.php page.
+ * Exit if called directly.
  */
-function GHL_wp49_css_fix() {
-    global $wp_version;
-
-    if ( version_compare( $wp_version, '4.9', '>=' ) ) {
-        ?>
-        <style type="text/css">
-            .plugins .plugin-title .github-link.dashicons::before {
-                background-color: transparent;
-                box-shadow: none;
-                font-size: 16px;
-                color: inherit;
-            }
-        </style>
-        <?php
-    }
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
+
+class GitHub_Link {
+
+    /**
+     * GitHub_Link constructor.
+     */
+    private function __construct() {
+    }
+
+    /**
+     * GitHub_Link init function.
+     */
+    public static function init() {
+      $self = new self();
+      $self->init_hooks();
+    }
+
+
+    function init_hooks() {
+        include_once ABSPATH . '/wp-admin/includes/plugin.php';
+
+        $this->extra_plugin_headers = [ 'GitHub Plugin URI', 'GitLab Plugin URI', 'Bitbucket Plugin URI' ];
+
+        add_filter( "extra_plugin_headers", [ &$this, "extra_plugin_headers" ] );
+
+        $installed_plugins = get_plugins();
+
+        foreach ( $installed_plugins as $plugin_slug => $plugin_data ) {
+          add_filter( "plugin_action_links_{$plugin_slug}", [ &$this, 'plugin_action_links' ], 1000, 4 );
+          add_filter( "network_admin_plugin_action_links_{$plugin_slug}", [ &$this, 'plugin_action_links' ], 1000, 4 );
+        }
+
+        load_plugin_textdomain( 'github-link', false, dirname( __FILE__ ) . '/languages' );
+    }
+
+    /**
+     * Add custom git plugin headers
+     *
+     * @param $extra_headers
+     *
+     * @return array
+     */
+    function extra_plugin_headers( $extra_headers ) {
+        return $extra_headers + $this->extra_plugin_headers;
+    }
+
+    /**
+    * Add an additional element to the plugin action links.
+    *
+    * @param $actions
+    * @param $plugin_file
+    * @param $plugin_data
+    * @param $context
+    *
+    * @return array
+    */
+    function plugin_action_links( $actions, $plugin_file, $plugin_data, $context ) {
+
+        // No GitHub data during search installed plugins.
+        if ( 'search' === $context ) {
+          return $actions;
+        }
+
+        $link_template = '<a href="%s" title="%s" target="_blank" style="color: #32373c;"><img src="%s" style="width: 16px; height: 16px; margin-top: 4px; padding-right: 4px; float: none;" height="16" width="16" alt="%s" />%s</a>';
+
+        $branch = ! empty( $plugin_data['branch'] ) ? $plugin_data['branch'] : 'master';
+
+        foreach ( $this->extra_plugin_headers as $header ) {
+          if ( ! empty( $plugin_data[ $header ] ) ) {
+            $githost_name = preg_replace( '/ Plugin URI$/', '', $header );
+            $new_action   = [
+              strtolower( $githost_name ) => sprintf(
+                $link_template,
+                $plugin_data[ $header ],
+                __( 'Visit ' . $githost_name . ' repository', 'github-link' ),
+                plugins_url( 'icon/' . $githost_name . '-Mark-32px.png', __FILE__ ),
+                $githost_name,
+                $branch ? '/' . $branch : ''
+              ),
+            ];
+            $actions      = $new_action + $actions;
+          }
+        }
+
+        $plugin_page = $this->get_plugin_page( $plugin_file );
+        if ( false !== $plugin_page ) {
+          $new_action  = [
+            'wordpress_org' => sprintf(
+              $link_template,
+              $plugin_page,
+              __( 'Visit WordPress.org Plugin Page', 'github-link' ),
+              plugins_url( 'icon/' . 'wordpress-logo-32.png', __FILE__ ),
+              'wp_org',
+              ''
+            ),
+          ];
+            $actions = $new_action + $actions;
+        }
+
+        return $actions;
+    }
+
+    /**
+     * Check if a plugin is a official WP.org plugin.
+     *
+     * @param string $slug       eg askimet/askimet.php
+     * @return boolean $on_wporg True if is on WP.org
+     */
+    function is_on_wporg( $slug ) {
+        $on_wporg = false;
+        _maybe_update_plugins();
+        $plugin_state = get_site_transient( 'update_plugins' );
+        if ( isset( $plugin_state->response[ $slug ] )
+        || isset( $plugin_state->no_update[ $slug ] )
+        ) {
+          $on_wporg = true;
+        }
+        return $on_wporg;
+    }
+
+    /**
+     * Get WP.org plugin page.
+     *
+     * @param string $slug eg askimet/askimet.php
+     * @return mixed $plugin_page URL if it has a WP.org page (default: false)
+     */
+    function get_plugin_page( $slug ) {
+        $on_wporg = $this->is_on_wporg( $slug );
+        if ( $on_wporg ) {
+          $plugin_page = '';
+          _maybe_update_plugins();
+          $plugin_state = get_site_transient( 'update_plugins' );
+          if ( isset( $plugin_state->response[ $slug ] )
+          && property_exists( $plugin_state->response[ $slug ], 'url' )
+          ) {
+              $plugin_page = $plugin_state->response[ $slug ]->url;
+          } elseif ( isset( $plugin_state->no_update[ $slug ] )
+              && property_exists( $plugin_state->no_update[ $slug ], 'url' )
+            ) {
+              $plugin_page = $plugin_state->no_update[ $slug ]->url;
+          }
+          return $plugin_page;
+        }
+        return false;
+    }
+
+}
+
+GitHub_Link::init();
